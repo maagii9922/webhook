74a120024f089a5e7e9c949bf8c415a2e625ffc8
mandah <mandakh@bumanit.mn>
2021-04-09 00:55:27 +0800

    aa

--- gettingstarted/views.py
+++ gettingstarted/views.py
@@ -74,6 +74,11 @@ def post_facebook_message(fbid, recevied_message):
     joke_text = 'Yo ' + user_details['first_name'] + '..! ' + joke_text
 
 
+    post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s' % PAGE_ACCESS_TOKEN
+    response_msg = json.dumps(
+        {"recipient": {"id": fbid}, "message": {"text": joke_text}})
+    status = requests.post(post_message_url, headers={
+                           "Content-Type": "application/json"}, data=response_msg)
 
     print(joke_text)
     return HttpResponse(joke_text)
